f14d017e2f27cb8ae3061c6f304e83c7f3aad818
waps12b <waps12b@naver.com>
2016-06-14 16:22:19 +0900

    Update emiyamulzomdao-arduino.ino

--- emiyamulzomdao-arduino.ino
+++ emiyamulzomdao-arduino.ino
@@ -1,64 +1,107 @@
 #include <SHT1x.h>
-#include "ArduinoThread/Thread.h"
-#include "ArduinoThread/ThreadController.h"
-
+ 
 #define DataPin 14
 #define ClockPin 15
-
+ 
 SHT1x sht1x(DataPin, ClockPin);
+ 
+#define AVG_CNT 3
+#define PUMP_CNT 4
 
-float temp;
-float humid;
-int photo;
-int water;
-int soil;
+int temp;   //temperature
+int humid;  //humidity
+int photo;  //
+int soil;   //humidity of soil
+int water;  //
 int motorIN1 = 11;
 int motorIN2 = 10;
-
-void setup() {
+int pump_timer = 4;
+ 
+void setup() 
+{
   Serial.begin(115200);
   pinMode(motorIN1, OUTPUT);
   pinMode(motorIN2, OUTPUT);
 }
-
-
+ 
+//센서값 read
 void ReadValue() {
   temp = sht1x.readTemperatureC() - 5;
   humid = sht1x.readHumidity();
   photo = analogRead(0);
-  water = digitalRead(2);
   soil = analogRead(2);
+  water = digitalRead(2);
 }
 
+void SendValue(){
+   Serial.print(photo); Serial.print("  ");
+   Serial.print(temp); Serial.print("  ");
+   Serial.print(humid); Serial.print("  ");
+   Serial.print(soil); Serial.print("  ");
+   Serial.print(water);
+   Serial.print("\n");
+}
+ 
 //펌프 실행 함수
-void pump(){
-    digitalWrite(motorIN1, HIGH);
-    digitalWrite(motorIN2, LOW);
-    delay(3000);
-    digitalWrite(motorIN1, LOW);
-    digitalWrite(motorIN2, LOW);
+void pump()
+{  
+    if(pump_timer > 0 )
+    {  //if pump timer is on
+       pump_timer --;
+    }
+    
+    //Serial.write(pump_timer);
+    if(pump_timer == 0)
+    {     //pump turn off pump
+          digitalWrite(motorIN1, LOW);
+          digitalWrite(motorIN2, LOW);
+    }else
+    {   //turn off
+         digitalWrite(motorIN1, HIGH);
+         digitalWrite(motorIN2, LOW);
+    }
 }
+ 
+void loop() 
+{
+  //if some data is on serial
+  while(Serial.available())
+  {   
+      //read command byte from Serial
+      char command  = Serial.read();
 
-void loop() {
-  float sum_t = 0;
-  float sum_h = 0;
-  float sum_pd = 0;
-  float sum_soil = 0;
+      switch(command)
+      {
+        //sensor data request command 
+        case 0x01: 
+          ReadValue();
+          SendValue();
+          break;
 
-  //센서 평균값 계산
-  for (int i = 0 ; i < 3 ; i++) {
-    ReadValue();
-    sum_t += temp;
-    sum_h += humid;
-    sum_pd += (float)photo;
-    sum_soil += (float)soil;
+        //PUMP on command
+        case 0x02:
+          pump_timer = PUMP_CNT;  
+          break;
+
+        //PUMP off command
+        case 0x03:
+          pump_timer = 0;
+
+          break;
+
+        default:
+          command = 0x00;
+          break;
+      }
+
+      //re-send received command to ecube
+      //if invalid command received, send 0x00
+      Serial.write(command);
   }
-  
-                                                          
-  Serial.print(sum_t / 3); Serial.print("  ");
-  Serial.print(sum_h / 3); Serial.print("  ");
-  Serial.print(sum_pd / 3); Serial.print("  ");
-  Serial.print(water); Serial.print("  ");  //water값이 1일경우 물부족
-  Serial.println(sum_soil / 3);              //값이 낮을수록 수분이 많음
 
+  //run pump cycle
+  pump();
+
+  //delay 1 second
+  delay(1000);
 }
